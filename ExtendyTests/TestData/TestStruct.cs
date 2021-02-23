0d68682e164aeaf9c2a77abdda507b54f336dde2
james.pfluger <me@jamespfluger.com>
2021-02-22 23:32:49 -0800

    Add AppendMany

---
+++ ExtendyTests/TestData/TestStruct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtendyTests.TestData
+{
+    public struct TestStruct
+    {
+        public int Id { get; set; }
+        public decimal DecimalValue { get; set; }
+        public char CharValue { get; set; }
+    }
+}
