1345c25cb7bda679948480a0709661698eff220c
james.pfluger <me@jamespfluger.com>
2021-02-23 18:21:25 -0800

    Add NextBool for Random extensions

---
+++ Extendy/Misc/RandomExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extendy.Misc
+{
+    public static class RandomExtensions
+    {
+        /// <summary>
+        /// Returns a random boolean.
+        /// </summary>
+        /// <returns>A random boolean value.</returns>
+        public static bool NextBool(this Random source)
+        {
+            return source.Next(2) == 0;
+        }
+
+        /// <summary>
+        /// Returns a random boolean weighted on the probability of the boolean being true.
+        /// </summary>
+        /// <param name="percentage">The probability as a percentage between 0->100 of a random boolean being true.</param>
+        /// <returns>Either true or false, based on the probability provided.</returns>
+        public static bool NextBool(this Random source, double percentage)
+        {
+            return source.NextDouble() < percentage / 100.0;
+        }
+    }
+}
---
+++ ExtendyTests/Misc/RandomExtensionsTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Extendy.Misc;
+using Xunit;
+
+namespace ExtendyTests.Misc
+{
+    public class RandomExtensionsTests
+    {
+        [Fact(DisplayName = "NextBool")]
+        public void NextBoolTest()
+        {
+            // Manually specifying a seed guarantees the Random object generates the same data
+            Random generator = new Random(100);
+
+            Assert.False(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.False(generator.NextBool());
+            Assert.False(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.False(generator.NextBool());
+            Assert.False(generator.NextBool());
+            Assert.False(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.True(generator.NextBool());
+        }
+
+        [Fact(DisplayName = "NextBool - with probability")]
+        public void NextBoolProbabilityTest()
+        {
+            // Manually specifying a seed guarantees the Random object generates the same data
+            Random generator = new Random(100);
+
+            // Check for high probability of a true value
+            for (int i=0; i<100; i++)
+                Assert.True(generator.NextBool(99.99));
+
+            // Check for low probability of a true value
+            for (int i=0; i<100; i++)
+                Assert.False(generator.NextBool(0.01));
+
+            // Check for medium probability of a true value
+            Assert.False(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.False(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.False(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.True(generator.NextBool());
+            Assert.False(generator.NextBool());
+        }
+    }
+}
